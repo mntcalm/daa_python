bf3df9f594e34453747236bc4844a11aa3b290ca
mntcalm <mntcalm@gmail.com>
2023-11-08 16:51:44 +0200

    added trainee dir

---
+++ beginner/4/4.py
@@ -0,0 +1,5 @@
+
+lst=["asdfg","ds","psd price ---","zdfg","vsfdaafsdv"]
+
+print([ x.upper() if "price" in x else x for x in lst])
+
---
+++ beginner/7/7.py
@@ -0,0 +1,3 @@
+sps=['my_string1','my_string2','my_string3']
+
+print(','.join(sps))
