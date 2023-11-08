4c1ce8bd2043e81a8e9bb802e7b036bad85123a1
mntcalm <mntcalm@gmail.com>
2023-11-08 17:10:00 +0200

    homeworks

--- 7/7.py
+++
@@ -1,3 +0,0 @@
-sps=['my_string1','my_string2','my_string3']
-
-print(','.join(sps))
--- 9/9.py
+++
@@ -1,9 +0,0 @@
-def cleanr(sps, sps_cl):
-   for j in range (0, len(sps_cl)):
-       sps.remove(sps_cl[j])
-   return sps
-
-list1 = [1, 2, 3, 4, 5]
-list2 = [1, 3, 4]
-
-print(cleanr(list1, list2))
